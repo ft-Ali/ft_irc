9c46692f7f959464cd3908c2d15b624f8051fefa
Felipe <plangloi@student.42.fr>
2024-12-10 16:56:22 +0100

    class channel config

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -1,7 +1,13 @@
 #include "../inc/Server.hpp"
+#include <map>
 
 class Channel{
 	private:
-		
+		std::string _name;
+		std::string _topic;
+		std::vector< Client> _members;
+		//modes
+	public:
+		Channel(const std::string &channelName) : _name(channelName), _topic(""){};
 
 };
