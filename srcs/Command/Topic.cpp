fe5b872832801ef9125a6a9d1f2aa8911e75e039
Louis Merveilleux du vignaux <lmerveil@bess-f3r5s6.clusters.42paris.fr>
2024-12-26 18:04:45 +0100

    topic cmd missing send

--- srcs/Command/Topic.cpp
+++ srcs/Command/Topic.cpp
@@ -1,18 +1,11 @@
 
-// returns the #Channelname topic
+// /topic #channel                  -> return current topic
+// /topic #channel : "new topic"    -> set new topic
 
-///TOPIC #channelname :New topic text
-// -> sets the topic to :...
 
 #include "../../inc/Channel.hpp"
 #include "../../inc/Server.hpp"
 
-//1. recup channelName
-//2. check operator
-//3. set TOPIC
-
-
-//TOPIC #channelname
 std::string cutCmdInMessage(std::string message, std::string cmd) {
     
     size_t hashPos = message.find('#', cmd.size());
@@ -24,22 +17,38 @@ void    Server::handleTopic(int clientFd, std::string message) {
 
     (void)clientFd;
 
+    //get cmd from message
     std::string cmd = message.substr(0, message.find(' '));
-    std::cout << cmd << std::endl;
+    std::cout << "cmd: [" << cmd << "] --size: " << cmd.size() << std::endl;
+
+    //get channelName from message
     std::string channelName = cutCmdInMessage(message, cmd);
-    std::cout << channelName << std::endl;
-    std::string newTopic = message.substr(cmd.size()+channelName.size()+2);
+    std::cout << "channelName: [" << channelName << "] --size: " << channelName.size() << std::endl;
+    
+    //get newTopic from message
+    std::string newTopic = message.substr(cmd.size()+1+channelName.size(), message.size()); // +1 for space between cmd and channelName
+    std::cout << "newTopic: [" << newTopic << "] --size: " << newTopic.size() << std::endl;
 
-    std::cout << "newTopic: [" << newTopic << "]\n";
-
+    //check if channelName exists
+    int idx = 0;
+    std::cout << message << "\n";
     if (newTopic.empty()) {
-        int idx = 0;
-
-        while (_channels[idx]->getName() != channelName)
-            idx++;
-        if (_channels[idx]->checkOperatorList(_clients[clientFd - 4])) {
-            std::cout << _channels[idx]->getTopic() << std::endl;
-        }
+        std::cout << "-> channel " << channelName << "'s CURRENT topic: [" << _channels[idx]->getTopic() << "]\n";
     }
+    while (_channels[idx] && _channels[idx]->getName() != channelName)
+        std::cout << idx++ << std::endl;
+    
+    //if topic is empty and current client is operator then set NEW topic
+    if (_channels[idx]->getTopic().empty()) {
+        _channels[idx]->setTopic(_clients[clientFd - 4], newTopic);
+        std::cout << "-> channel " << channelName << "'s NEW topic: [" << _channels[idx]->getTopic() << "]\r\n";
+    } else { //else if topic is NOT empty return current topic
+        std::string sentence = "-> channel "+ channelName + "'s CURRENT topic: [" + _channels[idx]->getTopic() + "]\r\n";
+        const void *buff = sentence.c_str();
 
+        std::cout << "-> channel " << channelName << "'s CURRENT topic: [" << _channels[idx]->getTopic() << "]\n";
+        int returnVal = send(clientFd, buff, _channels[idx]->getTopic().size(), 0);
+        std::cout << "returnVal: [" << returnVal << "]";
+    }
 }
+
